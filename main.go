0a4b42f88bda1a59adf0cf6d23f12e4dfaf1959d
손진영 <jysohn0825@naver.com>
2021-11-25 07:54:41 +0000

    동기, 비동기 고루틴 확인

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "./practice"
 
 func main(){
-	practice.GoroutinePractice()
+	practice.ChannelPractice()
 }
--- practice/18_channel.go
+++ practice/18_channel.go
@@ -1,19 +1,52 @@
 package practice
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func ChannelPractice(){
-	var str = "Hello World!"
-	
+func syncPractice(){
 	done := make(chan bool)
-	
+
 	go func() {
-		for i := 0; i < 10; i++ {
-			fmt.Println(str, i)	
+		for i := 0; i < 4; i++ {
+			done <- true
+
+			fmt.Println("고루틴 : ", i)
 		}
-		
-		done <- true //채널에 true를 송신함
 	}()
 
-	<- done //수신함으로써 대기를 끝냄
+	for i := 0; i < 4; i++ {
+		<-done
+		
+		fmt.Println("메인 함수 : ", i)
+		
+		time.Sleep(time.Second) //눈에 보이게 하기 위해 넣음
+	}	
+}
+
+
+func asyncPractice(){
+	done := make(chan bool, 2)
+
+	go func() {
+		for i := 0; i < 6; i++ {
+			done <- true
+
+			fmt.Println("고루틴 : ", i)
+		}
+	}()
+
+	for i := 0; i < 6; i++ {
+		<-done                    
+		
+		fmt.Println("메인 함수 : ", i)
+	}	
+}
+
+func ChannelPractice(){
+	fmt.Println("동기 시작")
+	syncPractice()
+	fmt.Println("\n\n비동기 시작")
+	asyncPractice()
 }
