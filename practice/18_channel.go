bdd16e7008475b2f2a81800c36efa52eb3a3dadc
손진영 <jysohn0825@naver.com>
2021-12-14 08:10:02 +0000

    상품 관리 코드 추가

--- practice/18_channel.go
+++ practice/18_channel.go
@@ -30,14 +30,14 @@ func asyncPractice(){
 	done := make(chan bool, 2)
 
 	go func() {
-		for i := 0; i < 6; i++ {
+		for i := 0; i < 4; i++ {
 			done <- true
 
 			fmt.Println("고루틴 : ", i)
 		}
 	}()
 
-	for i := 0; i < 6; i++ {
+	for i := 0; i < 4; i++ {
 		<-done                    
 		
 		fmt.Println("메인 함수 : ", i)
